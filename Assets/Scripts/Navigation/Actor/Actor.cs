781fa982d61846907318337f79bae2e3315cdcda
Maciej Bodziak <maciej.bodziak@gmail.com>
2024-11-22 12:47:19 +0100

    change the rotation of the actor from constant speed to interpolation

--- Assets/Scripts/Navigation/Actor/Actor.cs
+++ Assets/Scripts/Navigation/Actor/Actor.cs
@@ -12,9 +12,12 @@ namespace Navigation
         [ReadOnly][SerializeField] NavGrid _grid;
         [ReadOnly][SerializeField] int _currentNodeIndex;
         int _previousNodeIndex = -1;
-        [SerializeField] float _speed;
-        [SerializeField] float _speedModifier;
-        [SerializeField] float _rotationSpeed;
+        [Tooltip("world units per second")]
+        [SerializeField] float _speed = 1;
+        [Tooltip("movement speed is multiplied by it. Default should be 1")]
+        [SerializeField] float _speedModifier = 1;
+        [Tooltip("Inverse of time needed to reach target rotation. 1 means it takes 1 second to reach new rotation.")]
+        [SerializeField] float _rotationSpeed = 4;
         bool _cancelFlag = false;
         [ReadOnly][SerializeField] ActorState _state = ActorState.Uninitilized;
         Path _path;
@@ -108,6 +111,9 @@ namespace Navigation
         {
             OnMovementStarted();
 
+            Quaternion previousRotation = transform.rotation;
+            float rotationProgress = 0f;
+
             while (_pathIndex < _path.Count)
             {
                 _previousNodeIndex = _currentNodeIndex;
@@ -115,16 +121,18 @@ namespace Navigation
                 OnNodeExiting();
 
                 _targetPosition = _path[_pathIndex].worldPosition;
-                _targetRotation = Quaternion.LookRotation(_targetPosition - transform.position);
+
+                UpdateRotationTarget(ref rotationProgress, ref previousRotation);
+
 
                 while (transform.position != _targetPosition)
                 {
                     if (_state == ActorState.Moving)
                     {
                         float delta = _speed * _speedModifier * Time.deltaTime;
-                        float rotationDelta = _rotationSpeed * _speedModifier * Time.deltaTime;
+                        rotationProgress += _rotationSpeed * _speedModifier * Time.deltaTime;
                         transform.position = Vector3.MoveTowards(transform.position, _targetPosition, delta);
-                        transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, rotationDelta);
+                        transform.rotation = Quaternion.Slerp(previousRotation, _targetRotation, rotationProgress);
                     }
                     yield return null;
                 }
@@ -142,6 +150,19 @@ namespace Navigation
             MovementFinishedEvent?.Invoke(this, new ActorFinishedMovementEventArgs(_currentNodeIndex));
         }
 
+        private void UpdateRotationTarget(ref float rotationProgress, ref Quaternion previousRotation)
+        {
+            Quaternion nextTargetRotation = Quaternion.LookRotation(_targetPosition - transform.position);
+
+            //use Dot Product to check if the target Rotation has changed, and if so reset rotation progress
+            if (Mathf.Abs(Quaternion.Dot(_targetRotation, nextTargetRotation)) < 0.999f)
+            {
+                _targetRotation = nextTargetRotation;
+                rotationProgress = 0f;
+                previousRotation = transform.rotation;
+            }
+
+        }
 
         public void Teleport(int nodeIndex)
         {
@@ -201,12 +222,12 @@ namespace Navigation
             _state = ActorState.Moving;
             Quaternion startRotation = transform.rotation;
             Quaternion targetRotation = Quaternion.LookRotation(worldPosition - transform.position);
-            float progress = _speedModifier * Time.deltaTime * 2f;
+            float progress = _rotationSpeed * _speedModifier * Time.deltaTime;
 
             while (progress < 1)
             {
                 transform.rotation = Quaternion.Slerp(startRotation, targetRotation, progress);
-                progress += _speedModifier * Time.deltaTime * 2f;
+                progress += _rotationSpeed * _speedModifier * Time.deltaTime;
                 yield return null;
             }
             _state = ActorState.Idle;
